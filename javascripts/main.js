0774e0697dad033c7dfbac25353ef33cc35174bb
darrenmwong <darrenwong06@gmail.com>
2014-04-08 11:58:21 -0700

    added functionality of hover jquery to all projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,26 @@
 console.log('WOO');
 
 $('.seatProject').hover(function() {
-    "use stritct";
-    $('.projectImage').css('opacity', '.6');
+    "use strict";
+    $('#seatImage').css('opacity', '.6');
 }, function() {
     "use strict";
-    $('.projectImage').css('opacity', '').css('max-width', '');
+    $('#seatImage').css('opacity', '').css('max-width', '');
      });
+
+$('.comparisonProject').hover(function() {
+    "use strict";
+    $('#comparisonImage').css('opacity', '.6');
+}, function() {
+    "use strict";
+    $('#comparisonImage').css('opacity', '');
+    });
+
+$('.pawProject').hover(function() {
+    "uses strict";
+    $('#pawImage').css('opacity', '.6');
+}, function() {
+    "use strict";
+    $('#pawImage').css('opacity', '');
+    });
 
