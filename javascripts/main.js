940662c76e73effa27a0718b30cb903637d9d4b4
darrenmwong <darrenwong06@gmail.com>
2014-04-08 11:51:09 -0700

    added javascript to images hover

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,25 +1,10 @@
-// Variables
-var viewport = $(window),
-    root = $('html'),
-    maxScroll;
+console.log('WOO');
 
-// Bind events to window
-viewport.on({
-  scroll: function() {
-    // Grab scroll position
-    var scrolled = viewport.scrollTop();
-
-    /**
-     * Calculate our factor, setting it as the root `font-size`.
-     *
-     * Our factor is calculated by multiplying the ratio of the page scrolled by our base factor. The higher the base factor, the larger the parallax effect.
-     */
-    root.css({ fontSize: (scrolled / maxScroll) * 50 });
-  },
-  resize: function() {
-    // Calculate the maximum scroll position
-    maxScroll = root.height() - viewport.height();
-  }
-}).trigger('resize');
-
+$('.seatProject').hover(function() {
+    "use stritct";
+    $('.projectImage').css('opacity', '.6');
+}, function() {
+    "use strict";
+    $('.projectImage').css('opacity', '').css('max-width', '');
+     });
 
