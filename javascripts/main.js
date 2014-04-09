ef76d6ae9945b5f686a9055a5c8ee02b5b8e7932
darrenmwong <darrenwong06@gmail.com>
2014-04-09 15:59:47 -0700

    refactored containers for projects

--- javascripts/main.js
+++ javascripts/main.js
@@ -48,3 +48,19 @@ $('.navClose').hover(function() {
     console.log('touched');
     $('.line, .box').slideUp('slow');
     });
+
+/* SCROLL FUNCTION */
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
